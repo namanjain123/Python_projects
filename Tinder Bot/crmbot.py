19a1a74df9f18a788bb40d8e12891d62add21173
namanjain123 <naman.nishi@gmail.com>
2022-11-30 18:44:56 +0530

    new files and old kaggle and other projects

---
+++ Tinder Bot/crmbot.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+class crmbot():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+    #def login(self):
+     #   self.driver.get('https://ctcmswebapp.azurewebsites.net/task/CreateAccount')
+      #  account_name=self.driver.find_element_by_xpath('/html/body/div/div/div/div/div[2]/div/form/div[1]/div[1]/div/div/input')
+       # account_name.send('naman')
+        #account_owner=self.driver.find_element_by_xpath()
+        #website=self.driver.find_element_by_xpath()
+       # unit=self.driver.find_element_by_xpath()
+       # subunit=self.driver.find_element_by_xpath()
+        #subindustry=self.driver.find_element_by_xpath()
