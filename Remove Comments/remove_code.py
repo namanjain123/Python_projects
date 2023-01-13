fb2423dde4a36d020069fe68e69ad7df4887dd44
namanjain123 <naman.nishi@gmail.com>
2023-01-13 21:32:59 +0530

    Addtion to handle python file

--- Remove Comments/remove_code.py
+++ Remove Comments/remove_code.py
@@ -1,8 +1,15 @@
 # Import the file to delete the code
 import json
+import re
 address=input("please enter the address of the code")
 if(address.__contains__(".json")):
     with open('{address}', 'r') as handle:
         fixed_json = ''.join(line for line in handle if not line.startswith('//'))
         employee_data = json.loads(fixed_json)
-    print(employee_data)
+        print(employee_data)
+if address.__contains__(".py"):
+    with open('{address}', 'r') as handle:
+        code = str(handle)
+        print(re.sub(r'(?m)^ *#.*\n?', '', code))
+    
+
