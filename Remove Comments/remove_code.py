c0ca437d74b9851746809e6593d97dc0a28bbfc0
namanjain123 <naman.nishi@gmail.com>
2023-01-17 23:41:57 +0530

    Handle c# code

--- Remove Comments/remove_code.py
+++ Remove Comments/remove_code.py
@@ -11,5 +11,16 @@ if address.__contains__(".py"):
     with open('{address}', 'r') as handle:
         code = str(handle)
         print(re.sub(r'(?m)^ *#.*\n?', '', code))
+if address.__contains__(".cs"):
+    with open('{address}', 'r') as handle:
+        pattern = r'//.*'
+        # remove single-line comments
+        handle = re.sub(pattern, '', handle)
+        # regular expression pattern to match multi-line comments
+        pattern = r'/\*.*?\*/'
+        # remove multi-line comments
+        handle = re.sub(pattern, '', handle, flags=re.DOTALL)
+        print(code)
+
     
 
