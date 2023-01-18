f762bc31c173f45b056014b34c7f1c6a0e916485
namanjain123 <naman.nishi@gmail.com>
2023-01-18 17:13:03 +0530

    Addition to remove comment from the c++ file handled

--- Remove Comments/remove_code.py
+++ Remove Comments/remove_code.py
@@ -4,6 +4,7 @@ import re
 address=input("please enter the address of the code")
 if(address.__contains__(".json")):
     with open('{address}', 'r') as handle:
+        handle = str(handle)
         fixed_json = ''.join(line for line in handle if not line.startswith('//'))
         employee_data = json.loads(fixed_json)
         print(employee_data)
@@ -13,6 +14,7 @@ if address.__contains__(".py"):
         print(re.sub(r'(?m)^ *#.*\n?', '', code))
 if address.__contains__(".cs"):
     with open('{address}', 'r') as handle:
+        handle = str(handle)
         pattern = r'//.*'
         # remove single-line comments
         handle = re.sub(pattern, '', handle)
@@ -20,7 +22,12 @@ if address.__contains__(".cs"):
         pattern = r'/\*.*?\*/'
         # remove multi-line comments
         handle = re.sub(pattern, '', handle, flags=re.DOTALL)
-        print(code)
+        print(handle)
+if address.__contains__(".cpp"):
+    with open('{address}', 'r') as handle:
+        handle = str(handle)
+        handle=re.sub(r"(?://.*)|(/\*(.|\n)*?\*/)", "", handle)
+        print(handle)
 
     
 
