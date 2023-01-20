8d32ee17965d9b7996b7a412584d4d7907ab604e
namanjain123 <naman.nishi@gmail.com>
2023-01-20 23:30:29 +0530

    Addition of new project that need to handle diffrent text

---
+++ change_the_text_to_desired.py
@@ -0,0 +1,8 @@
+print("Please enter the test to change")
+name=input()
+print("enter text to replace \n")
+text=input()
+print("enter the text replaced by \n")
+to_text=input()
+name=name.replace(text, to_text)
+print(name)
